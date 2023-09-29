2cf6c2427699fcfc27439df1b4aed1ce6c492b55
Denis <denisbutko2003@gmail.com>
2023-09-30 00:07:42 +0300

    published_add_intex_in_to_root

--- src/js/modules/showShop.js
+++ src/js/modules/showShop.js
@@ -2,14 +2,11 @@ const showShop = () =>{
 	window.addEventListener("click", (e) =>{
 		const shopList = document.querySelector(".shoppingCard");
 		if(e.target.classList.contains("button") || e.target.classList.contains("shopping-card") ){
-			console.log('open');
 			shopList.classList.remove("right-[-100%]");
 			shopList.classList.add("right-[0]");
 		}else if(e.target.classList.contains("close-shopping-card")){
 			shopList.classList.add("right-[-100%]");
 			shopList.classList.remove("right-[0]");
-
-			console.log('close');
 		}
 	});
 }
