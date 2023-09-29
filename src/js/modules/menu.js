c32cb95ea7660112970c65c93ed6c77d5af743cd
Denis <denisbutko2003@gmail.com>
2023-09-29 23:31:58 +0300

    published

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -4,14 +4,6 @@ const menu = () =>{
 	const parent = document.querySelector(".products");
 	const menu = document.querySelector(".menu");
 	
-	
-
-		menu.classList.remove("hidden");
-		menu.classList.remove("left-[-100%]");
-		menu.classList.add("left-0");
-		menuButton.classList.add("rotate-180");
-	
-		
 		parent.addEventListener('click', function handleClick(e) {
 			if (e.target.classList.contains('rotate-180') && e.target.classList.contains('menu__button')) {
 				menu.classList.add("hidden");
