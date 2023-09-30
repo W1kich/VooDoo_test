ca1dd5877baee4adb8b07c31cfb1e0031396a9f0
Denis <denisbutko2003@gmail.com>
2023-09-30 11:29:21 +0300

    published_add_transition

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,19 +1,23 @@
 
-const menu = () =>{
+const menu = async () =>{
 	const menuButton = document.querySelector(".menu__button");
 	const parent = document.querySelector(".products");
 	const menu = document.querySelector(".menu");
-	
+	const menuHeight = await menu.clientHeight;
+	menu.classList.add(`h-[0]`);
+	console.log(menuHeight);
 		parent.addEventListener('click', function handleClick(e) {
 			if (e.target.classList.contains('rotate-180') && e.target.classList.contains('menu__button')) {
-				menu.classList.add("hidden");
+				menu.classList.add(`h-[0]`);
+				menu.classList.remove(`h-[${menuHeight}px]`);
 				menu.classList.remove("left-0");
-				menu.classList.add("left-[-100%]");
+				menu.classList.add("left-[-9999px]");
 				menuButton.classList.remove("rotate-180");
 			} else if(!e.target.classList.contains('rotate-180') && e.target.classList.contains('menu__button')){
-				menu.classList.remove("hidden");
+				menu.classList.add(`h-[${menuHeight}px]`);
+				menu.classList.remove(`h-[0]`);
 				menu.classList.add("left-0");
-				menu.classList.remove("left-[-100%]");
+				menu.classList.remove("left-[-9999px]");
 				menuButton.classList.add("rotate-180");
 			}
 		});
